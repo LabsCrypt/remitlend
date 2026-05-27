fe805f09238fbd061da4f09e0996e0319b3acf4a
artylobos <artylobos@users.noreply.github.com>
2026-05-27 19:35:39 +1000

    frontend: fix service worker manifest typing

--- frontend/src/app/sw.ts
+++ frontend/src/app/sw.ts
@@ -2,24 +2,16 @@ import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry } from "@serwist/precaching";
 import { Serwist } from "serwist";
 
-declare const self: ServiceWorkerGlobalScopeEventMap;
+declare const self: WorkerGlobalScope & {
+  __SW_MANIFEST: PrecacheEntry[];
+};
 
 const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST as PrecacheEntry[],
+  precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
-  bypassCdn: ({ request }) => {
-    if (
-      request.url.includes("/api/") ||
-      request.url.includes("/sse/") ||
-      request.url.includes("/_next/")
-    ) {
-      return true;
-    }
-    return false;
-  },
 });
 
 serwist.addEventListeners();
